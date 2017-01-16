7f7e02d2b08675fbced6a194af3afaaadf78044a
frankburke333 <frank.burke@gmail.com>
2017-01-16 15:02:47 -0500

    add gets.chomp for name and greet using str-intpln

--- exercise3.rb
+++ exercise3.rb
@@ -1,1 +1,6 @@
+#using gets.chomp to get user input to store in a variable.  using gets on it's own adds a line so I didn't use it
+
 puts "What is your name?"
+name = gets.chomp
+
+puts "Hi #{name}!!"
