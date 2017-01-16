33b89a031fbe32c7e10f8d90da281e3eeb97e16c
frankburke333 <frank.burke@gmail.com>
2017-01-16 14:38:47 -0500

    add completed exercise2

---
+++ exercise2.rb
@@ -0,0 +1,13 @@
+#multiply the total by 1.20
+
+puts 55 * 1.20
+
+#adding string to interger using .to_i
+puts "three" + "#{3.to_i}"
+
+
+#using string interpolation to add calculation result
+puts "357 million be all like #{45628 * 7839}"
+
+#this is true because the second is NOT true.
+(10 < 20 && 30 < 20) || !(10 == 11)
